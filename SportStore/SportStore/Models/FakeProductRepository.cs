45495820656a4b88081764146ffbdc55bbe254b0
mariojgas <33085529+mariojgas@users.noreply.github.com>
2017-11-02 10:13:12 +0000

    Update Product/ Added Fake Product Repository
    updated Product class and added fake product repository class

---
+++ SportStore/SportStore/Models/FakeProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeProductRepository:IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product> {
+            new Product{Name ="Football",Price=25,Description="Sport Ball"},
+            new Product{Name="Surf Board",Price=95 }
+        };
+    }
+}
--- SportStore/SportStore/Models/Product.cs
+++ SportStore/SportStore/Models/Product.cs
@@ -9,6 +9,9 @@ namespace SportStore.Models
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-        
+        public int ProductID { get; set; }
+        public string Description{ get; set; }
+
+        public string Category { get; set; }
     }
 }
